53b21983d1b1419e51822445f0a82af8930dfce8
殷琪瑶 <yinqiyao99@foxmail.com>
2022-09-07 18:01:19 +0800

    首页

--- src/views/home/components/HomeTab.vue
+++ src/views/home/components/HomeTab.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <van-tabs v-model="active">
+    <van-tabs v-model="activeName" @click="tabClick"  >
       <van-tab 
         v-for="(item,index) in hometabs" 
         :key="index" 
         :title="item.title"
-        @click="tabClick(index)"  
+        :name="item.type"
       >
       <div 
         v-for="(item1,index1) in item.list" 
         :key="index1"
         class="tab-item"
+        ref="goodsList"
       >
         <img :src="item1.show.img" alt="">
         <p class="title">{{item1.title}}</p>
@@ -28,45 +29,57 @@ export default {
   },
   data () {
     return {
-      hometabs:{
-        "pop":{
+      hometabs:[
+        {
+          type: "pop",
           title: "流行",
           page: 1,
           list:[]
         },
-        "new":{
+        {
+          type: "new",
           title: "新款",
           page: 1,
           list: []
         },
-        "sell":{
+        {
+          type: "sell",
           title: "精选",
           page: 1,
           list: []
         }
-      }, // 分类数据
-      active: 0,
-      currentType: "pop"
+      ], // 分类数据
+      activeName: "pop",
     };
   },
   methods: {
-    getTabList(type) {
+    tabClick(name) {
+
+    },
+    async getTabList(item) {
       const params = {
-        type: type,
-        page: this.hometabs[type].page
+        type: item.type,
+        page: item.page
       };
-      _getHomeGoods(params).then(res => {
-        this.hometabs[type].list = res.data.list
-      })
+      await _getHomeGoods(params).then(res => {
+        item.list = res.data.list
+      });
+    },
+    lazyLoading(e) {
+      // const scrollTop = e.taregt.scrollTop
+      // const  
     }
   },
   watch: {},
   computed: {
   },
-  created() {
-    for(let item in this.hometabs) {
+   created() {
+    for(let item of this.hometabs) {
       this.getTabList(item)
     }
+  },
+  mounted() {
+    window.addEventListener('scroll',this.lazyLoading)
   }
 }
 </script>
@@ -75,17 +88,17 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-  padding: 10px 0px;
+  padding: 10px 10px;
 }
 .tab-item {
-  width: 45%;
-  height: auto;
   margin-bottom: 20px;
   img {
-    width: 100%;
+    width: 160px;
+    height: 208px;
     border-radius: 10px;
   }
   .title {
+    width: 160px;
     font-size: 14px;
     overflow: hidden;
     text-overflow: ellipsis;
